bf8617475ba65298d6bc6090de7e48d0ccfbef84
Shazmina Khan <141507954+Shazminaa@users.noreply.github.com>
2024-07-31 17:48:08 -0700

    practice of multi child widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MaterialApp(
+  runApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blueGrey.shade50,
         appBar: AppBar(
           backgroundColor: Colors.blue.shade200,
           title: const Text('Dashboard'),
         ),
-        body: Center(
-          child: Container(
-          height: 300,
-          width: 300,
-          color: Color.fromARGB(255, 187, 113, 138),
-          child: Center(
-            child: Text('Hey its my first app'),
-          ),
-          )
-          
-        )
+        body: Column(
+          children: const [
+            Center(
+              child: Text('Center Widget'),
+            ),
+            Text('First Widget'),
+            Text('Second Widget'),
+            Text('Third Widget'),
+            Text('Fourth Widget'),
+          ],
+        ),
       ),
     ),
   );
